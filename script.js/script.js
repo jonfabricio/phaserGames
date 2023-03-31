e8a59b358c5ae0489478c2e43bdfa21cec95a54d
Jojow-World <jonatas-fab@hotmail.com>
2023-03-31 18:07:13 +0100

    animação

--- script.js/script.js
+++ script.js/script.js
@@ -1,7 +1,9 @@
 var config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  scale: {
+    width: 800,
+    height: 600,
+},
   physics: {
     default: "arcade",
     arcade: {
@@ -14,26 +16,12 @@ var config = {
     create: create,
     update: update,
   },
-  scoreboard: {
-    width: 25,
-    base: "number",
-    number0: "number0",
-    number1: "number1",
-    number2: "number2",
-    number3: "number3",
-    number4: "number4",
-    number5: "number5",
-    number6: "number6",
-    number7: "number7",
-    number8: "number8",
-    number9: "number9",
-  },
+ 
 };
 
 var game = new Phaser.Game(config);
-var tubo;
-var tubos;
 
+// music, animations...
 function preload() {
   //mapa
   this.load.image('mapa', 'assets/mapa3.jpg');
@@ -46,7 +34,7 @@ function preload() {
   this.load.image('tuboInvertido', 'assets/tubo-invertido.png');
 
   //restart
-  this.load.image("restart", "assets/restart-button.png");
+  this.load.image('restart', "assets/restart-button.png");
   
   //pontuação
   this.load.image("num0","assets/number0.png");
@@ -59,42 +47,64 @@ function preload() {
   this.load.image("num7", "assets/number7.png");
   this.load.image("num8", "assets/number8.png");
   this.load.image("num9", "assets/number9.png");
+
+     
+
 }
 
+
+
+var score = 0;
+var scoreText;
+var tubo;
+var tubos;
+var jack;
+
+
+
+
+// interações
 function create() {
+
   this.add.image(400,300, 'mapa');
-  this.add.image(400,300, 'jack');
-  
-  this.physics.world.setBounds(0,0,game.config.width,game.config.height);
 
-  tubos = this.physics.add.group();
-  // cria o tubo de baixo
-  tubo = tubos.create(game.config.width / 2, game.config.height - 25, 'tubo');
-  tubo.setScale(0.145);
-  // setOrigin(0, 1) é importante para definir o ponto de ancoragem do sprite no canto inferior esquerdo, para que a posição do tubo seja definida pelo seu canto inferior esquerdo
-  tubo.setOrigin(0.5, 1);
-  // setImmovable(true) também é importante para que o tubo não se mova quando colidir com outros objetos.
-  tubo.setCollideWorldBounds(false);
-  tubo.setImmovable(true);	
-  tubo.body.setAllowGravity(false);
+//cria os tubos
+tubos = this.physics.add.group();
+
+  // movimentação personagem
+  jack = this.physics.add.sprite(100,game.config.height/2,'jack');
+  jack.setScale(1.8);
+  
+this.input.on(
+  "pointerdown",
+  function () {
+    jack.setVelocityY(-400);
+  },
+  this
+);
+
+
+//   this.tubo = this.physics.sprite(400,300, 'tubo');
+//   tubo.setScale(0.145);
+//   this.tubo.setVelocityX(125);
+ 
+//   // // cria o tubo de cima
+//  tuboInvertido = tubos.create(game.config.width / 2, 25, 'tuboInvertido');
+//  tuboInvertido.setScale(0.130);
+//    tuboInvertido.setOrigin(0.5, 0);
+//    tuboInvertido.setCollideWorldBounds(true);
+
   
 
-  // cria o tubo de cima
-  tuboInvertido = tubos.create(game.config.width / 2, 25, 'tuboInvertido');
-  tuboInvertido.setScale(0.130);
-  tuboInvertido.setOrigin(0.5, 0);
-  tuboInvertido.setCollideWorldBounds(true);
-  tuboInvertido.setImmovable(true);
-  tuboInvertido.body.setAllowGravity(false);
-  
 
-  // adiciona os tubos ao grupo
-  tubos.add(tubo);
-  tubos.add(tuboInvertido);
+   
+
 }
 
 	
 
 
 
-function update() {}
+function update() {
+      
+}
